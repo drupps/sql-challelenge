ac26da66294f26e15972695874511a9ca92eade5
Dave Ruppel <drupps@gmail.com>
2020-10-06 11:15:58 -0700

    updating schema

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,68 +1,43 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/6MQx2P
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-drop table if exists employees, departemnts, dept_employees, dept_manager, salaries, titles cascade;
+﻿drop table if exists employees, departments, dept_employees, dept_manager, salaries, titles CASCADE;
+-- titles done
+CREATE TABLE "titles" (
+    "title_id" varchar(10) primary key NOT NULL,
+    "title" varchar(30)   NOT NULL
+);
 
+CREATE TABLE "departments" (
+    "dept_no" varchar(10)  primary key NOT NULL,
+    "dept_name" varchar(30)   NOT NULL
+);
 
 CREATE TABLE "employees" (
-    "emp_no" int   NOT NULL,
+    "emp_no" int primary key NOT NULL,
     "emp_title_id" varchar(15)   NOT NULL,
     "birth_date" varchar(15)   NOT NULL,
     "first_name" varchar(30)   NOT NULL,
     "last_name" varchar(30)   NOT NULL,
     "sex" varchar(2)   NOT NULL,
     "hire_date" varchar(10)   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
-CREATE TABLE "departments" (
-    "dept_no" varchar(10)   NOT NULL,
-    "dept_name" varchar(30)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+    foreign key (emp_title_id) references titles(title_id)
 );
 
 CREATE TABLE "dept_employees" (
     "emp_no" int   NOT NULL,
     "dept_no" varchar(10)   NOT NULL,
-    CONSTRAINT "pk_dept_employees" PRIMARY KEY (
-        "dept_no"
-     )
+    foreign key (emp_no) references employees(emp_no),
+    foreign key (dept_no) references departments(dept_no)
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" varchar(10)   NOT NULL,
-    "emp_no" int   NOT NULL
+    "emp_no" int   NOT NULL,
+    foreign key (emp_no) references employees(emp_no),
+    foreign key (dept_no) references departments(dept_no)
 );
 
 CREATE TABLE "salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL
+    "salary" int   NOT NULL,
+    foreign key (emp_no) references employees(emp_no)
 );
-
-CREATE TABLE "titles" (
-    "title_id" varchar(10)   NOT NULL,
-    "title" varchar(30)   NOT NULL
-);
-
-ALTER TABLE "dept_employees" ADD CONSTRAINT "fk_dept_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_employees" ADD CONSTRAINT "fk_dept_employees_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "employees" ("emp_title_id");
 
